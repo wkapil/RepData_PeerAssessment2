f9c708235d9e9c597eb7367f361bb0ca0a22a785
Kapil Wadodkar <w.kapil@gmail.com>
2014-10-24 01:02:08 +0530

    corrected the graph orientation to improve visibility of lables.

--- StromCode.R
+++ StromCode.R
@@ -1,54 +1,54 @@
-download.file("https://d396qusza40orc.cloudfront.net/repdata%2Fdata%2FStormData.csv.bz2", 
-              "repdata_data_StormData.csv.bz2")
+download.file("https://d396qusza40orc.cloudfront.net/repdata%2Fdata%2FstormData.csv.bz2", 
+              "repdata_data_stormData.csv.bz2")
 
-csv <- bzfile("repdata_data_StormData.csv.bz2", "repdata_data_StormData.csv")
-stormdata <- read.csv2(csv, sep = ",", stringsAsFactors = FALSE)
-unlink(csv)
+baseData <- bzfile("repdata_data_stormData.csv.bz2", "repdata_data_stormData.csv")
+stormData <- read.csv2(baseData, sep = ",", stringsAsFactors = FALSE)
+unlink(baseData)
 
-str(stormdata)
+str(stormData)
 
-summary(stormdata)
+summary(stormData)
 
-head(stormdata)
+head(stormData)
 
-stormdata$FATALITIES <- as.numeric(stormdata$FATALITIES)
+stormData$FATALITIES <- as.numeric(stormData$FATALITIES)
 
-stormdata$INJURIES <- as.numeric(stormdata$INJURIES)
+stormData$INJURIES <- as.numeric(stormData$INJURIES)
 
-stormdata$EVTYPE <- toupper(stormdata$EVTYPE)
-eventtype <- sort(unique(stormdata$EVTYPE))
+stormData$EVTYPE <- toupper(stormData$EVTYPE)
+eventType <- sort(unique(stormData$EVTYPE))
 ## Show first 50 event types
-eventtype[1:50]
+eventType[1:50]
 
 library(data.table)
 
-stormdata$EVTYPE <- as.factor(stormdata$EVTYPE)
+stormData$EVTYPE <- as.factor(stormData$EVTYPE)
 
-fatalities <- as.data.table(subset(aggregate(FATALITIES ~ EVTYPE, data = stormdata, 
+fatalities <- as.data.table(subset(aggregate(FATALITIES ~ EVTYPE, data = stormData, 
                                              FUN = "sum"), FATALITIES > 0))
 fatalities <- fatalities[order(-FATALITIES), ]
 
-top20 <- fatalities[1:20, ]
+top20Fatalities <- fatalities[1:20, ]
 library(ggplot2)
-ggplot(data = top20, aes(EVTYPE, FATALITIES, fill = FATALITIES)) + geom_bar(stat = "identity") + 
-  xlab("Event") + ylab("Fatalities") + ggtitle("Fatalities caused by Events (top 20) ") + 
-   theme(legend.position = "none")
+ggplot(data = top20Fatalities, aes(EVTYPE, FATALITIES, fill = FATALITIES)) + geom_bar(stat = "identity") + 
+  xlab("Event") + ylab("Fatalities") + coord_flip() + ggtitle("Fatalities caused by Events (top 20) ") + 
+  theme(legend.position = "none")
 
-injuries <- as.data.table(subset(aggregate(INJURIES ~ EVTYPE, data = stormdata, 
+injuries <- as.data.table(subset(aggregate(INJURIES ~ EVTYPE, data = stormData, 
                                            FUN = "sum"), INJURIES > 0))
 injuries <- injuries[order(-INJURIES), ]
 
-top20i <- injuries[1:20, ]
-ggplot(data = top20i, aes(EVTYPE, INJURIES, fill = INJURIES)) + geom_bar(stat = "identity") + 
-  xlab("Event") + ylab("Injuries") + ggtitle("Injuries caused by Events (top 20) ") + 
-   theme(legend.position = "none")
+top20Injuries <- injuries[1:20, ]
+ggplot(data = top20Injuries, aes(EVTYPE, INJURIES, fill = INJURIES)) + geom_bar(stat = "identity") + 
+  xlab("Event") + ylab("Injuries") + coord_flip() + ggtitle("Injuries caused by Events (top 20) ") + 
+  theme(legend.position = "none")
 
-unique(stormdata$PROPDMGEXP)
+unique(stormData$PROPDMGEXP)
 
-stormdata$PROPDMGEXP <- toupper(stormdata$PROPDMGEXP)
-unique(stormdata$PROPDMGEXP)
+stormData$PROPDMGEXP <- toupper(stormData$PROPDMGEXP)
+unique(stormData$PROPDMGEXP)
 
-table(stormdata$PROPDMGEXP)
+table(stormData$PROPDMGEXP)
 
 calcExp <- function(x, exp = "") {
   switch(exp, `-` = x * -1, `?` = x, `+` = x, `1` = x, `2` = x * (10^2), `3` = x * 
@@ -67,15 +67,15 @@ applyCalcExp <- function(vx, vexp) {
   result
 }
 
-stormdata$EconomicCosts <- applyCalcExp(as.numeric(stormdata$PROPDMG), stormdata$PROPDMGEXP)
-summary(stormdata$EconomicCosts)
+stormData$EconomicCosts <- applyCalcExp(as.numeric(stormData$PROPDMG), stormData$PROPDMGEXP)
+summary(stormData$EconomicCosts)
 
-costs <- as.data.table(subset(aggregate(EconomicCosts ~ EVTYPE, data = stormdata, 
+costs <- as.data.table(subset(aggregate(EconomicCosts ~ EVTYPE, data = stormData, 
                                         FUN = "sum"), EconomicCosts > 0))
 costs <- costs[order(-EconomicCosts), ]
 
 library(scales)
-top20c <- costs[1:20, ]
-ggplot(data = top20c, aes(EVTYPE, EconomicCosts, fill = EconomicCosts)) + geom_bar(stat = "identity") + 
-  scale_y_continuous(labels = comma) + xlab("Event") + ylab("Economic costs in $") + 
+top20Costs <- costs[1:20, ]
+ggplot(data = top20Costs, aes(EVTYPE, EconomicCosts, fill = EconomicCosts)) + geom_bar(stat = "identity") + 
+  scale_y_continuous(labels = comma) + xlab("Event") + ylab("Economic costs in $") + coord_flip() +
   ggtitle("Economic costs caused by Events (top 20) ") +  theme(legend.position = "none")
